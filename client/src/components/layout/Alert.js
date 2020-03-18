060af935f67c80791a2323909c4905cab2e25d50
Ely Fuentes <ely.fuentes@tsia.com>
2020-03-18 14:43:34 -0700

    Added Edit Profile Functionality and UI

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-const Alert = ({ alerts }) =>
-  alerts !== null &&
-  alerts.length > 0 &&
-  alerts.map(alert => (
-    <div key={alert.id} className={`alert alert-${alert.alertType}`}>
-      {alert.msg}
-    </div>
-  ));
+const Alert = ({ alerts }) => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [alerts]);
+
+  return (
+    alerts !== null &&
+    alerts.length > 0 &&
+    alerts.map(alert => (
+      <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+        {alert.msg}
+      </div>
+    ))
+  );
+};
 
 Alert.propTypes = {
   alerts: PropTypes.array.isRequired
