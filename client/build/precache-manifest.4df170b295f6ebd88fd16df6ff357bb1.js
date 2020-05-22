a8288cc3ebb010229fcbfda8d181a2d93d7e3782
ely.fuentes@tsia.com <ely.fuentes@tsia.com>
2020-05-22 15:00:49 -0700

    Testing Npm Build

---
+++ client/build/precache-manifest.4df170b295f6ebd88fd16df6ff357bb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1ee837e29d6266c293c4dd1cc0eda7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b74e31f86f75b40d4645",
+    "url": "/static/css/main.b9988ad9.chunk.css"
+  },
+  {
+    "revision": "300398b477f2a8fefd64",
+    "url": "/static/js/2.832b8bcb.chunk.js"
+  },
+  {
+    "revision": "b74e31f86f75b40d4645",
+    "url": "/static/js/main.ab53371c.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "523316a56995c03edf34fffddf6e1193",
+    "url": "/static/media/showcase.523316a5.jpg"
+  },
+  {
+    "revision": "2d77c2dbd65f9538c8514eebecffefee",
+    "url": "/static/media/spinner.2d77c2db.gif"
+  }
+]);
