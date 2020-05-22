5fa5f27ca2a3804ca4a8d6631c1b6ab09068267c
Ely Fuentes <ely.fuentes@tsia.com>
2020-05-22 12:49:16 -0700

    Prepering for deployment

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
-//Connect Database 
+//Connect Database
 connectDB();
 
 //Init Middleware
 
-app.use(express.json({
-    extended: false
-}))
+app.use(
+  express.json({
+    extended: false,
+  })
+);
 
-app.get('/', (req, res) => res.send('API Running'));
+// Remove This so it runs in aws
+// app.get('/', (req, res) => res.send('API Running'));
 
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -20,6 +24,16 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static(''));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server Started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
