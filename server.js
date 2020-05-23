56a12afc0168e2c3a0265d22f336b1ee3eccaa39
ely.fuentes@tsia.com <ely.fuentes@tsia.com>
2020-05-22 21:38:25 -0700

    fix static folder

--- server.js
+++ server.js
@@ -24,10 +24,10 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-//Serve static assets in production
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  //set static folder
-  app.use(express.static(''));
+  // Set static folder
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
